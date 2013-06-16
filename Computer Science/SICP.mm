7a2e811965f4f2f0911a5fc46d3a8504dac9d3a2
Fungo Wang <lonelyibex@gmail.com>
2013-06-16 23:32:50 +0800

    add mind map of some books   APUE   CSAPP   SICP   TCPL-C++   Version Control with Git

---
+++ Computer Science/SICP.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1371394514646" ID="ID_586450138" MODIFIED="1371394579897" TEXT="SICP(Structure and Interpretation of Computer Programs)">
+<node CREATED="1371394609258" ID="ID_938946547" MODIFIED="1371394684906" POSITION="right" TEXT="1. Building  Abstractions with Procedures"/>
+<node CREATED="1371394650302" ID="ID_1523018709" MODIFIED="1371394671246" POSITION="right" TEXT="2. Building Abstractions with Data"/>
+<node CREATED="1371394687725" ID="ID_1158775455" MODIFIED="1371394704593" POSITION="right" TEXT="3. Modularity, Objects, and State"/>
+<node CREATED="1371394705049" ID="ID_817427331" MODIFIED="1371394724984" POSITION="right" TEXT="4. Metalinguistic Abstraction"/>
+<node CREATED="1371394726473" ID="ID_400808282" MODIFIED="1371394748345" POSITION="right" TEXT="5. Computing with Register Machines"/>
+</node>
+</map>
